7f47fbf52922da4c20a7e413a3a3a2b90fff38e0
SonAlex <150028352+sonle05@users.noreply.github.com>
2026-04-28 21:12:45 +0700

    Refactor adapter.py for improved database connection

--- module-1-inventory-sync/adapter.py
+++ module-1-inventory-sync/adapter.py
@@ -15,7 +15,7 @@ os.makedirs(PROCESSED_DIR, exist_ok=True)
 db_config = {
     'user': 'root',
     'password': 'password',  # Khớp với MYSQL_ROOT_PASSWORD trong compose
-    'host': 'mysql_db',      # Tên service trong docker-compose
+    'host': 'module-sync',      # Tên service trong docker-compose
     'database': 'noah_retail', # Tên DB trong compose
     'port': 3306
 }
